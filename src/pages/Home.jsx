39aa96adc6ff3604a126d1061590ff2fd9c8f1d8
Cris Correa <cristina.correa.segade@gmail.com>
2025-05-14 14:39:48 +0100

    logo update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import ButtonGradient from "../assets/svg/ButtonGradient";
-import Collaboration from "../components/Collaboration";
 import Hero from "../components/Hero";
 import Roadmap from "../components/Roadmap";
-import Services from "../components/Services";
 import ContactForm from "../components/ContactForm";
 
 const Home = () => {
@@ -10,8 +8,6 @@ const Home = () => {
     <>
       <main className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Hero />
-        <Collaboration />
-        <Services />
         <Roadmap />
         <ContactForm />
       </main>
